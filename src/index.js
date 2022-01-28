52f0aee9134ee95a48358b481c4bc7f12be92ae3
jspark <sylvan0212@gmail.com>
2022-01-28 20:25:24 +0900

    22

--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ app.get("/users", function (req, res, next) {
 
 app.get("/users/:id", (req, res) => {
   const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
   const user = users.filter((user) => user.id === id)[0];
+  if (!user) return res.status(404).end();
   console.log({ user });
   res.json(user);
 });
