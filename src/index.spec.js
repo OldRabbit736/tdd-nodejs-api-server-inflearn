7f3fbd454dee918b03d503f21600f88e09c41412
jspark <sylvan0212@gmail.com>
2022-01-31 18:19:18 +0900

    27

--- src/index.spec.js
+++ src/index.spec.js
@@ -88,4 +88,16 @@ describe("POST /users는", () => {
       body.should.have.property("name", name);
     });
   });
+  describe("실패 시", () => {
+    it("name 파라미터 누락 시 400을 반환한다", (done) => {
+      request(app).post("/users").send({}).expect(400).end(done);
+    });
+    it("name이 중복일 경우 409를 반환한다", (done) => {
+      request(app)
+        .post("/users")
+        .send({ name: "daniel" })
+        .expect(409)
+        .end(done);
+    });
+  });
 });
